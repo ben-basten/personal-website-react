97c493e0851135a84f7765cbc802ea5bc802a656
Ben Basten <ben.basten@gmail.com>
2020-03-29 15:17:33 -0500

    Minor nav bar updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,14 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return(
-            <ul>
-                <li><a href="/">home</a></li>
-                <li><a href="/media">media</a></li>
-                <li><a href="/code">code</a></li>
-                <li><a href="/contact">contact</a></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href="/">home</a></li>
+                    <li><a href="/media">media</a></li>
+                    <li><a href="/code">code</a></li>
+                    <li><a href="/contact">contact</a></li>
+                </ul>
+            </nav>
         );
     }
 }
