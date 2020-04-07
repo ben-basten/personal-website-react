d83743a7978084e91a4643469dcabaef0a395c12
Ben Basten <ben.basten@gmail.com>
2020-04-07 13:26:22 -0500

    Layed out body structure of page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header/Header';
-import Sidebar from '../components/Sidebar/Sidebar';
+import Sidebar from './Sidebar/PageTitle';
+import Socials from './Sidebar/Socials';
 import Home from '../pages/home';
 import Media from '../pages/media';
 import Code from '../pages/code';
@@ -14,14 +15,17 @@ class App extends React.Component {
         return (
             <BrowserRouter>
                 <Header />
-                <Sidebar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/media" component={Media} />
-                    <Route exact path="/code" component={Code} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route component={NotFound} />
-                </Switch>
+                <div className="body">
+                    <Sidebar />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/media" component={Media} />
+                        <Route exact path="/code" component={Code} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Route component={NotFound} />
+                    </Switch>
+                    <Socials />
+                </div>
             </BrowserRouter>
         );
     }
---
+++ src/components/Sidebar/Socials.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Socials = () => (
+    <a href="http://instagram.com/ben.basten2">Instagram</a>
+);
+
+export default Socials;
