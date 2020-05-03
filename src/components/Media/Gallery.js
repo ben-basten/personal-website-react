3799357f485d7d459e8ec749d3b6cb24abeb75ec
Ben Basten <ben.basten@gmail.com>
2020-05-03 00:39:05 -0500

    Gallery dynamically fetches images from directory

--- src/components/Media/Gallery.js
+++ src/components/Media/Gallery.js
@@ -1,27 +1,25 @@
 import React from 'react';
 
 class Gallery extends React.Component {
+    generateImages = r => { 
+        let images = {};
+        r.keys().map((item, index) => { return images[item.replace('./', '')] = r(item); });
+        // images object contains objects in format of {[filename]: [path]}
+
+        let gallery = [];
+        for (const filename in images) { // iterates through the file paths to generate the gallery JSX
+            var name = filename.substring(0, filename.length - 4); // alt text is the filename minus the extension
+            gallery.push(<div className="image" key={filename}><img src={images[filename]} alt={name}/></div>) 
+        }
+
+        return gallery;
+    }
+
     render() {
         return(
             <div className="gallery">
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/belay.jpg")} alt="hope"/>
-                </div>
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/breakwall.jpg")} alt="hope"/>
-                </div>
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/campfire.jpg")} alt="hope"/>
-                </div>
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/campsunset.jpg")} alt="hope"/>
-                </div>
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/capitol.jpg")} alt="hope"/>
-                </div>
-                <div className="image">
-                    <img src={require("../../assets/images/gallery/cathedral.jpg")} alt="hope"/>
-                </div>
+                {/* require.context is a webpack feature to get modules from a directory */}
+                {this.generateImages(require.context('../../assets/images/gallery', false, /\.(png|jpe?g|svg)$/))} 
             </div>
         );
     }
