c68db6bcda96349f2d0933c22fb0a0953fb063eb
Ben - School F2019 <ben.basten@gmail.com>
2020-05-23 19:07:32 -0500

    slightly nicer yet still mediocre notification

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,7 @@ class Home extends React.Component {
     render() {
         return (
             <div className="content">
-                <h2>Site is currently under construction.</h2>
-                <p>Check back later for more updates!</p>
+                <h2 className="notify">Sorry, this site is currently under construction.</h2>
             </div>
         )
     }
